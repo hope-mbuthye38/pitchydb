0c95541dcc29a6427ab9d9cd8a36a0a92e2b05a5
hope-mbuthye38 <-C>
2021-09-26 06:35:42 +0300

    content

---
+++ config.py
@@ -0,0 +1,34 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    pass
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+
+    'development':DevConfig,
+    'production':ProdConfig,
+
+}
